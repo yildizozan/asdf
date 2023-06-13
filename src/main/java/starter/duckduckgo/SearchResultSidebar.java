53aad4e83d9008062929b69dff461bdae78fce28
Ozan YILDIZ <yildizozan@users.noreply.github.com>
2023-06-13 19:25:06 +0300

    Create SearchResultSidebar.java

---
+++ src/main/java/starter/duckduckgo/SearchResultSidebar.java
@@ -0,0 +1,7 @@
+import net.serenitybdd.core.pages.PageComponent;
+
+public class SearchResultSidebar extends PageComponent {
+    public String heading() {
+        return $(".module__title").getText();
+    }
+}
