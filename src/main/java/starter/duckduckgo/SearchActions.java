f0929413c784e533ed51bdd7dc6e9a703adc2f0f
Ozan YILDIZ <yildizozan@users.noreply.github.com>
2023-06-13 19:24:44 +0300

    Create SearchActions.java

---
+++ src/main/java/starter/duckduckgo/SearchActions.java
@@ -0,0 +1,8 @@
+import net.serenitybdd.core.steps.UIInteractions;
+
+public class SearchActions extends UIInteractions {
+    public void byKeyword(String keyword) {
+      $("#search_form_input_homepage").sendKeys(keyword);
+      $(".search__button").click();      
+    }
+}
