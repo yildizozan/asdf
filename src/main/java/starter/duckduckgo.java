6497c995624ebb54453c8a7ea6368b4e38631a9e
Ozan YILDIZ <yildizozan@users.noreply.github.com>
2023-06-13 19:22:09 +0300

    Create duckduckgo.java

---
+++ src/main/java/starter/duckduckgo.java
@@ -0,0 +1,14 @@
+package starter.duckduckgo;
+
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(SerenityJUnit5Extension.class)              
+class WhenSearchingByKeyword {              
+
+    @Test
+    void theKeywordShouldAppearInTheResultsSidebar() {
+
+    }
+}
